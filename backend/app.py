a99211746e8241b17659ea700832701783d471cb
venkatasai2006 <t.saibharadwaja@gmail.com>
2026-05-13 23:55:23 +0530

    Final Crime Lens AI Project

---
+++ backend/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from newspaper import Article
+from groq import Groq
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+client = Groq(
+    api_key="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"
+)
+
+@app.route("/")
+def home():
+    return "Crime Lens AI Backend Running 🚀"
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+
+    data = request.json
+    url = data.get("url")
+
+    article = Article(url)
+    article.download()
+    article.parse()
+
+    text = article.text
+
+    if len(text) > 4000:
+        text = text[:4000]
+
+    prompt = f"""
+Analyze this crime news article and return ONLY valid JSON.
+
+Format:
+{{
+    "crime_type": "",
+    "location": "",
+    "suspects": "",
+    "risk_level": "",
+    "date": "",
+    "summary": ""
+}}
+
+Article:
+{text}
+"""
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    ai_result = response.choices[0].message.content
+
+    cleaned = ai_result.replace("```json", "").replace("```", "").strip()
+
+    result_json = json.loads(cleaned)
+
+    return jsonify(result_json)
+
+if __name__ == "__main__":
+    app.run(debug=True)
